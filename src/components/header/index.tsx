d3124facf2a0d548e7db78bbe9b26fa261dc3d43
Kasili <63935096+kasiliwachiye@users.noreply.github.com>
2024-07-26 10:23:53 +0300

    fix: shut navbar upon navigation

--- src/components/header/index.tsx
+++ src/components/header/index.tsx
@@ -1,13 +1,28 @@
+// @components/header/index.tsx
 "use client";
 import styles from "./style.module.css";
 import Link from "next/link";
-import { useState } from "react";
+import { useState, useEffect } from "react";
+import { useRouter } from "next/router";
 import { AnimatePresence, motion } from "framer-motion";
 import { opacity, background } from "./anim";
 import Nav from "./nav";
 
 export default function Index() {
   const [isActive, setIsActive] = useState<boolean>(false);
+  const router = useRouter();
+
+  // Close the navbar on route change
+  useEffect(() => {
+    const handleRouteChange = () => {
+      setIsActive(false);
+    };
+
+    router.events.on("routeChangeStart", handleRouteChange);
+    return () => {
+      router.events.off("routeChangeStart", handleRouteChange);
+    };
+  }, [router.events]);
 
   return (
     <div className={styles.header}>
